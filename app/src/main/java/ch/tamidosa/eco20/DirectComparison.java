4e8ef44d7f70bc3b6498881dd4b8ee833a2fb0a1
Michael Keller <michaeke@student.ethz.ch>
2019-09-29 00:01:39 +0200

    direct comparison activity

---
+++ app/src/main/java/ch/tamidosa/eco20/DirectComparison.java
@@ -0,0 +1,14 @@
+package ch.tamidosa.eco20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DirectComparison extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direct_comparison);
+    }
+}
