8baec18fb72e2e1d0dbdf1471ac0aa5769580549
Tatyana Vogel <tvogel@adobe.com>
2019-09-28 17:40:34 +0200

    Add listview

--- app/src/main/java/ch/tamidosa/eco20/InfoPreviewActivity.java
+++ app/src/main/java/ch/tamidosa/eco20/InfoPreviewActivity.java
@@ -1,21 +1,29 @@
 package ch.tamidosa.eco20;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class InfoPreviewActivity extends AppCompatActivity {
 
     //Bool for FLAB pressed?
     private boolean isFABOpen = false;
+    private LinkedHashMap<String, GroupInfo> subjects = new LinkedHashMap<String, GroupInfo>();
+    private ArrayList<GroupInfo> deptList = new ArrayList<GroupInfo>();
+
+    private CustomAdapter listAdapter;
+    private ExpandableListView simpleExpandableListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,38 +32,62 @@ public class InfoPreviewActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //FLAB Stuff
         FloatingActionButton fab = findViewById(R.id.fab);
         FloatingActionButton fab1 = findViewById(R.id.fab1);
         FloatingActionButton fab2 = findViewById(R.id.fab2);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!isFABOpen){
+                if (!isFABOpen) {
                     showFABMenu();
-                }else{
+                } else {
                     closeFABMenu();
                 }
             }
         });
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab1 = (FloatingActionButton) findViewById(R.id.fab1);
-    fab2 = (FloatingActionButton) findViewById(R.id.fab2);
-    fab3 = (FloatingActionButton) findViewById(R.id.fab3);
-    fab.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(!isFABOpen){
-                showFABMenu();
-            }else{
-                closeFABMenu();
+        //expandable stuff
+        // add data for displaying in expandable list view
+        loadData();
+
+        //get reference of the ExpandableListView
+        simpleExpandableListView = (ExpandableListView) findViewById(R.id.simpleExpandableListView);
+        // create the adapter by passing your ArrayList data
+        listAdapter = new CustomAdapter(InfoPreviewActivity.this, deptList);
+        // attach the adapter to the expandable list view
+        simpleExpandableListView.setAdapter(listAdapter);
+
+        //expand all the Groups
+        expandAll();
+
+        // setOnChildClickListener listener for child row click
+        simpleExpandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                //get the group header
+                GroupInfo headerInfo = deptList.get(groupPosition);
+                //get the child info
+                ChildInfo detailInfo = headerInfo.getProductList().get(childPosition);
+                //display it or do something with it
+                Toast.makeText(getBaseContext(), " Clicked on :: " + headerInfo.getName()
+                        + "/" + detailInfo.getName(), Toast.LENGTH_LONG).show();
+                return false;
             }
-        }
-    });
+        });
+        // setOnGroupClickListener listener for group heading click
+        simpleExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                //get the group header
+                GroupInfo headerInfo = deptList.get(groupPosition);
+                //display it or do something with it
+                Toast.makeText(getBaseContext(), " Header is :: " + headerInfo.getName(),
+                        Toast.LENGTH_LONG).show();
 
-
-         */
+                return false;
+            }
+        });
     }
 
     private void showFABMenu(){
@@ -93,5 +125,69 @@ public class InfoPreviewActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //method to expand all groups
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            simpleExpandableListView.expandGroup(i);
+        }
+    }
+
+    //method to collapse all groups
+    private void collapseAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            simpleExpandableListView.collapseGroup(i);
+        }
+    }
+
+    //load some initial data into out list
+    private void loadData(){
+
+        addProduct("Weg","ListView");
+        addProduct("Weg","ExpandableListView");
+        addProduct("Weg","GridView");
+
+        addProduct("Koponente","PolyMorphism");
+        addProduct("Koponente","Collections");
+
+    }
+
+
+
+    //here we maintain our products in various departments
+    private int addProduct(String department, String product){
+
+        int groupPosition = 0;
+
+        //check the hash map if the group already exists
+        GroupInfo headerInfo = subjects.get(department);
+        //add the group if doesn't exists
+        if(headerInfo == null){
+            headerInfo = new GroupInfo();
+            headerInfo.setName(department);
+            subjects.put(department, headerInfo);
+            deptList.add(headerInfo);
+        }
+
+        //get the children for the group
+        ArrayList<ChildInfo> productList = headerInfo.getProductList();
+        //size of the children list
+        int listSize = productList.size();
+        //add to the counter
+        listSize++;
+
+        //create a new child and add that to the group
+        ChildInfo detailInfo = new ChildInfo();
+        detailInfo.setSequence(String.valueOf(listSize));
+        detailInfo.setName(product);
+        productList.add(detailInfo);
+        headerInfo.setProductList(productList);
+
+        //find the group position inside the list
+        groupPosition = deptList.indexOf(headerInfo);
+        return groupPosition;
     }
 }
