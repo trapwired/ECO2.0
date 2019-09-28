ea8cbedf222f92deaee066f48b5d0f4de0940c3a
Michael Keller <michaeke@student.ethz.ch>
2019-09-28 23:51:05 +0200

    family comparison activity

---
+++ app/src/main/java/ch/tamidosa/eco20/FamilyComparison.java
@@ -0,0 +1,14 @@
+package ch.tamidosa.eco20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FamilyComparison extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family_comparison);
+    }
+}
