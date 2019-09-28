ec95f207c4ea344fcb72da6bd1fbdeb8241b96ce
dominic <dominic.weibel@protonmail.ch>
2019-09-28 11:11:31 +0200

    First Commit

--- app/src/main/java/ch/tamidosa/eco20/MainActivity.java
+++ app/src/main/java/ch/tamidosa/eco20/MainActivity.java
@@ -14,6 +14,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Main Activity
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
