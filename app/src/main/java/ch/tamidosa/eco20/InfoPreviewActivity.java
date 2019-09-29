d63c7b0ad98beaf2780b1d0d42b1dddefd16ac71
Michael Keller <michaeke@student.ethz.ch>
2019-09-29 06:16:05 +0200

    once again i forgot about the importance of add

--- app/src/main/java/ch/tamidosa/eco20/InfoPreviewActivity.java
+++ app/src/main/java/ch/tamidosa/eco20/InfoPreviewActivity.java
@@ -151,20 +151,7 @@ public class InfoPreviewActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     //method to expand all groups
     private void expandAll() {
